72fb4f9fe81c5241a511e774a95bf0d5e96acc29
Nicolas "Norswap" Laurent <norswap@gmail.com>
2012-08-08 17:33:08 +0200

    Fixed a bug in the installation step (the value "Symbol's function definition is void: Configuration" would appear instead of the required "X86").

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -42,31 +42,37 @@ endif()
 
 if(WIN32)
 
-  set(_DEP_LINK_DIR "${CMAKE_SOURCE_DIR}/dep/lib/${PLATFORM}_${_CONFIG_NATIVE}")
+  # Where to find the precompiled dlls. The path is expressed differently at
+  # build time and install time (but stays the same).
+  set(_link_dir "${CMAKE_SOURCE_DIR}/dep/lib/${PLATFORM}_${_CONFIG_NATIVE}")
+  set(_inst_dir "${CMAKE_SOURCE_DIR}/dep/lib/${PLATFORM}_${_CONFIG_CMAKE}")
 
   # MySQL
   set(MYSQL_INCLUDE_DIR     "${CMAKE_SOURCE_DIR}/dep/include/mysql")
-  set(MYSQL_LIBRARY         "${_DEP_LINK_DIR}/libmySQL.lib")
+  set(MYSQL_LIBRARY         "${_link_dir}/libmySQL.lib")
+  set(_mysql_link_dll       "${_link_dir}/libmySQL.dll")
+  set(_mysql_inst_dll       "${_inst_dir}/libmySQL.dll")
+  set(_mysql_bt             "${BUILD_BINDIR_NATIVE}/libmySQL.dll")
 
   # OpenSSL
   set(OPENSSL_INCLUDE_DIR   "${CMAKE_SOURCE_DIR}/dep/include/openssl")
-  set(OPENSSL_LIBRARY       "${_DEP_LINK_DIR}/libeay32.lib")
+  set(OPENSSL_LIBRARY       "${_link_dir}/libeay32.lib")
+  set(_openssl_link_dll     "${_link_dir}/libeay32.dll")
+  set(_openssl_inst_dll     "${_inst_dir}/libeay32.dll")
+  set(_openssl_bt           "${BUILD_BINDIR_NATIVE}/libeay32.dll")
 
-  # A target that ensure a copy of the dll files into the build tree.
-  set(_MYSQL_BT   "${BUILD_BINDIR_NATIVE}/libmySQL.dll")
-  set(_OPENSSL_BT "${BUILD_BINDIR_NATIVE}/libeay32.dll")
-  _change_ext(_MYSQL_DLL   "${MYSQL_LIBRARY}"   dll)
-  _change_ext(_OPENSSL_DLL "${OPENSSL_LIBRARY}" dll)
+  # A target that ensure a copy of the dll files into the build tree (bt).
   add_custom_command(
-    OUTPUT  "${_MYSQL_BT}" "${_OPENSSL_BT}"
-    COMMAND "${CMAKE_COMMAND}" -E copy "${_MYSQL_DLL}" "${_MYSQL_BT}"
-    COMMAND "${CMAKE_COMMAND}" -E copy "${_OPENSSL_DLL}" "${_OPENSSL_BT}"
-    DEPENDS "${_MYSQL_DLL}" "${_OPENSSL_DLL}"
+    OUTPUT  "${_mysql_bt}" "${_openssl_bt}"
+    COMMAND "${CMAKE_COMMAND}" -E copy "${_mysql_link_dll}"   "${_mysql_bt}"
+    COMMAND "${CMAKE_COMMAND}" -E copy "${_openssl_link_dll}" "${_openssl_bt}"
+    DEPENDS "${_mysql_link_dll}" "${_openssl_link_dll}"
     VERBATIM)
-  add_custom_target(mysql_openssl ALL DEPENDS "${_MYSQL_BT}" "${_OPENSSL_BT}")
+  add_custom_target(mysql_openssl ALL DEPENDS "${_mysql_bt}" "${_openssl_bt}")
 
   # Install instruction for MySQL and OpenSSL Libraries.
-  install(FILES "${_MYSQL_DLL}" "${_OPENSSL_DLL}" DESTINATION "${BIN_DIR}")
+  install(FILES "${_mysql_inst_dll}" "${_openssl_inst_dll}"
+    DESTINATION "${BIN_DIR}")
 
 elseif(UNIX)
 
